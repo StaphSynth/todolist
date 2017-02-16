f65e3135d932e885353cafcfd21032b84d5c66f7
StaphSynth <StaphSynth@users.noreply.github.com>
2017-02-16 16:39:05 +1100

    playing with odering

--- todolist.js
+++ todolist.js
@@ -1,5 +1,32 @@
-var listApp = angular.module('todolist-app', []);
 
-listApp.controller('todolist-controller', function($scope){
+var listApp = angular.module('todolistApp', []);
 
-};//controller
+listApp.controller('todolistController', function(){
+  this.list = [
+    {
+      thing: 'feed the cat',
+      priority: 0
+    },
+    {
+      thing: 'wash the dishes',
+      priority: 2
+    },
+    {
+      thing: "learn angular",
+      priority: 1
+    },
+    {
+      thing: "get a job",
+      priority: 0
+    },
+    {
+      thing: "watch paint dry",
+      priority: 2
+    },
+    {
+      thing: "aardvarks: advance",
+      priority: 2
+    }
+  ];
+  this.order = 'priority';
+}); //controller
