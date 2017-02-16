f0365cd07776c37da41033f1bf27c2c08030f7bb
StaphSynth <StaphSynth@users.noreply.github.com>
2017-02-16 22:46:34 +1100

    add order by completion. Tidy namespace

--- todolist.js
+++ todolist.js
@@ -5,50 +5,50 @@ listApp.controller('todolistController', function(){
   var ctrl = this;
   ctrl.list = [
     {
-      thing: 'feed the cat',
+      task: 'feed the cat',
       priority: 0,
       complete: false
     },
     {
-      thing: 'wash the dishes',
+      task: 'wash the dishes',
       priority: 2,
       complete: true
     },
     {
-      thing: "learn angular",
+      task: "learn angular",
       priority: 1,
       complete: false
     },
     {
-      thing: "get a job",
+      task: "get a job",
       priority: 0,
       complete: false
     },
     {
-      thing: "watch paint dry",
+      task: "watch paint dry",
       priority: 2,
       complete: false
     },
     {
-      thing: "aardvarks: advance",
+      task: "aardvarks: advance",
       priority: 2,
       complete: true
     }
   ];
   ctrl.order = 'priority';
-  ctrl.userItem = null;
+  ctrl.userTask = null;
   ctrl.userPriority = 0;
 
   ctrl.add = function() {
-    if(ctrl.userItem == null)
+    if(ctrl.userTask == null)
       return false;
 
     priorityVal = parseInt(ctrl.userPriority);
     var item = {
-      thing: ctrl.userItem,
+      task: ctrl.userTask,
       priority: priorityVal
     };
-    ctrl.userItem = null;
+    ctrl.userTask = null;
     ctrl.list.push(item);
   };
 
