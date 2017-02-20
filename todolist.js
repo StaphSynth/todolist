b91748542818828e3016f83a2de8304774aad91c
Staphsynth <1337dallen@gmail.com>
2017-02-20 11:12:46 +1100

    remove find function, simplify editing, deleting and completion marking

--- todolist.js
+++ todolist.js
@@ -7,32 +7,38 @@ listApp.controller('todolistController', function(){
     {
       task: 'feed the cat',
       priority: 0,
-      complete: true
+      complete: true,
+      editing: true
     },
     {
       task: 'wash the dishes',
       priority: 2,
-      complete: true
+      complete: true,
+      editing: false
     },
     {
       task: "learn angular",
       priority: 1,
-      complete: false
+      complete: false,
+      editing: false
     },
     {
       task: "get a job",
       priority: 0,
-      complete: false
+      complete: false,
+      editing: false
     },
     {
       task: "watch paint dry",
       priority: 2,
-      complete: false
+      complete: false,
+      editing: false
     },
     {
       task: "aardvarks: advance",
       priority: 2,
-      complete: true
+      complete: true,
+      editing: false
     }
   ];
   ctrl.order = 'priority';
@@ -53,36 +59,29 @@ listApp.controller('todolistController', function(){
     ctrl.list.push(item);
   };
 
-  //returns the array index of the task found, else false.
-  ctrl.find = function(task) {
-    if(task) {
-      for(var i = 0; i < ctrl.list.length; i++) {
-        if(ctrl.list[i].task === task) {
-          return i;
-        }
+  //deletes a selected list item
+  ctrl.delete = function(item) {
+    for(var i = 0; i < ctrl.list.length; i++) {
+      if(ctrl.list[i].task === item.task) {
+        ctrl.list.splice(i, 1);
+        return true;
       }
-      return false;
     }
     return false;
   };
 
-  //deletes a selected list item
-  ctrl.delete = function(task) {
+  ctrl.complete = function(item) {
+    item.complete ? item.complete = false : item.complete = true;
+  };
+
+  ctrl.editTask = function(task) {
     if(task) {
-      ctrl.list.splice(ctrl.find(task), 1);
-      return true;
+      var index = ctrl.find(task);
+      console.log('Item editing: ', ctrl.list[index].task);
+      ctrl.list[index].editing = true;
     } else {
       return false;
     }
-  };
-
-  ctrl.complete = function(task) {
-    if(task) {
-      var index = ctrl.find(task);
-      ctrl.list[index].complete ? ctrl.list[index].complete = false : ctrl.list[index].complete = true;
-      return true;
-    }
-    return false;
   };
 
 }); //controller
